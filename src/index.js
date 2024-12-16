cf18d1fe6175acaf86f5679fc09803a014561685
fransua <fransua.ne@gmail.com>
2024-12-16 22:14:07 +0100

    add appStoreVersion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import * as core from "@actions/core";
+// import * as core from "@actions/core";
 import Utils from './utils.js'
+
+const core = {
+  getInput(name){
+    return ({
+      'app-id': '6737245898',
+      'key-id': 'KN5DQZNJCK',
+      'issuer-id': '70404d9c-c471-42e1-849d-f266781d74c3',
+      'private-key-p8-path': '/home/fransua/Downloads/AuthKey_KN5DQZNJCK.p8'
+    })[name];
+  },
+  setOutput: console.log
+}
 
 async function run() {
   const utils = new Utils()
@@ -10,6 +22,14 @@ async function run() {
   const privateKeyRaw = core.getInput('private-key-raw')
   const privateKeyFilePath = core.getInput('private-key-p8-path')
   const privateKeyFileBase64 = core.getInput('private-key-p8-base64')
+
+  const params = new URLSearchParams({
+    'fields[builds]': ['version'],
+    'fields[appStoreVersions]': ['versionString', 'appVersionState'],
+    'include': ['builds', 'appStoreVersions'],
+  })
+  // const url = `/v1/builds?${params.toString()}`
+  const url = `/v1/apps/${appId}?${params.toString()}`
   const tokenString = utils.getToken(
     appId,
     issuerId,
@@ -18,16 +38,26 @@ async function run() {
     privateKeyRaw,
     privateKeyFilePath,
     privateKeyFileBase64,
-    'GET /v1/builds?filter[app]={appId}')
+    `GET ${url}`)
 
-
-  const result = await fetch(`https://api.appstoreconnect.apple.com/v1/builds?filter[app]=${appId}`, {
+  const result = await fetch(`https://api.appstoreconnect.apple.com${url}`, {
     headers: {
       Authorization: `Bearer ${tokenString}`
     }
   }).then(x => x.json());
-  const version = Math.max(...result.data.map(x => x.attributes.version));
-  core.setOutput(`version`, version)
+  const builds = result.included.filter(x => x.type == 'builds').map(x => x.attributes);
+  const appStoreVersions = result.included.filter(x => x.type == 'appStoreVersions').map(x => x.attributes)
+      .sort((a,b) => a.versionString > b.versionString ? -1 : 1);
+  const version = Math.max(...builds.map(x => x.version));
+  const appStoreVersion = appStoreVersions[0];
+  if (appStoreVersion.appVersionState == 'READY_FOR_DISTRIBUTION') {
+    const semVer = appStoreVersion.versionString.split('.');
+    semVer[2] = +(semVer[2] ?? 0) + 1;
+    core.setOutput(`appStoreVersion`, semVer.join('.'));
+  } else {
+    core.setOutput(`appStoreVersion`, appStoreVersion.versionString);
+  }
+  core.setOutput(`version`, +(version ?? 0) + 1)
 }
 
 run()
